798c876d170d7b83bbe2fb1ac98c0c423592acf7
jamesloy98 <46301833+jamesloy98@users.noreply.github.com>
2021-11-18 11:40:01 -0600

    Add files via upload

---
+++ lambdaSort/src/lambdaSort/MainClass.java
@@ -0,0 +1,15 @@
+package lambdaSort;
+
+public class MainClass {
+	
+
+	public static void main(String[] args) {
+		
+		//We keep the main method clean by ONLY creating an object of the --
+		//ActionClass and then calling the method.
+		ActionClass runAction = new ActionClass();
+		runAction.sortText();
+		
+	}
+
+}
